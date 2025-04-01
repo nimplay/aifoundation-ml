471a45a66f783f7be7b9e6da9c64f5f88b2fb783
Nimrod Acosta <52327093+nimplay@users.noreply.github.com>
2025-03-31 20:49:22 -0400

    Discriminat analysis

---
+++ linear_discriminant_analysis.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+# Linear Regression Example
+""" Problem:
+Sarah's Challenge: Loves trying new fruits, but wants to increase her odds of finding winners.
+
+LDA to the Rescue: This technique helps predict "like" or "dislike" based on both size and sweetness.
+
+How LDA Works: It analyzes Sarah's past choices, lookimg for the best way to separete the fruits she's enjoyed from the ones she hasn't.
+
+Imagime drawing a line on the grahp to divide the "like" and "dislike" fruits as cleanly as possible.
+
+The power of Combination: LDA shines when multiple features matters. It helps Sarah use size and sweetness together for potencially better predictions.
+"""
+#import data
