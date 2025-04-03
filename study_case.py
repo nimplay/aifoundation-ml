b9ef262d606297de57cc7e07c96817e73b72f123
Nimrod Acosta <52327093+nimplay@users.noreply.github.com>
2025-04-03 18:12:29 -0400

    study case

--- study_case.py
+++ study_case.py
@@ -2,3 +2,15 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+"""
+Case Study:
+* Problem Definition
+* Independent Variable
+* Dependent Variable
+* Required Python Libraries
+* Data Loading
+* Data Processing
+* Missing Data Removal
+* Outlier Detection
+"""
